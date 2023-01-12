e671119b3094d05fee655191ac1a748236829829
Fabian Hintermayr <97839471+fhintermayr@users.noreply.github.com>
2023-01-12 20:04:45 +0100

    fix: it didn't include the id of the customer and the ablesung in the to string method

--- src/main/java/eu/bsinfo/gruppe4/model/Kunde.java
+++ src/main/java/eu/bsinfo/gruppe4/model/Kunde.java
@@ -3,14 +3,17 @@ package eu.bsinfo.gruppe4.model;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 import java.util.UUID;
 
 @Data
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@ToString(onlyExplicitlyIncluded = true)
 @NoArgsConstructor
 public class Kunde {
     @EqualsAndHashCode.Include
+    @ToString.Include
     private UUID id;
     private String name;
     private String vorname;
